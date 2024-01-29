e8f25f97fbc7980b0b0c6ff40401212762e7d5f0
Oleg-Kaniuk <129388390+Oleg-Kaniuk@users.noreply.github.com>
2024-01-29 14:51:40 +0000

    Deploying to gh-pages from @ Oleg-Kaniuk/final-frontend@047b95db6ef4932d3d08a13c1cd7ecafd0bfb3c5 🚀

---
+++ static/js/794.cc98c78d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[794],{1794:function(e,t,r){r.r(t);var a=r(7689),c=r(184);t.default=function(){return(0,c.jsx)(a.Fg,{to:"/login",replace:!0})}}}]);
+//# sourceMappingURL=794.cc98c78d.chunk.js.map
